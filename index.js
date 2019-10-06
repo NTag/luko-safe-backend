f785e0482cfbfd72c8e7d86726ab351a05ddee45
Basile Bruneau <basilebruneau@gmail.com>
2019-10-06 16:59:43 +0200

    Add id, fake estimatedValue and endWarrantyDate to item when adding one

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const sharp = require('sharp');
+const uuidv4 = require('uuid/v4');
+const moment = require('moment');
 const port = 8080;
 
 app.use(morgan('combined'));
@@ -21,6 +23,17 @@ const itemSummary = (item) => {
   delete item2.image;
   return item2;
 };
+const findItem = (id) => {
+  return items.find(item => item.id === id);
+};
+const itemWithDetails = (item) => {
+  const category = CATEGORIES.find(c => c.value === item.category);
+
+  return {
+    ...item,
+    category,
+  };
+};
 
 app.get('/', (req, res) => res.send());
 app.get('/items', (req, res) => res.send(items.map(itemSummary)));
@@ -29,10 +42,22 @@ app.post('/items', async (req, res) => {
   const item2 = { ...req.body };
   delete item2.image;
   console.log('new item', item2);
+
   const imageBase64 = req.body.image;
   const imageBuffer = new Buffer(imageBase64, 'base64');
   const thumbnail = (await sharp(imageBuffer).resize(400).toBuffer()).toString('base64');
-  const item = { ...req.body, thumbnail: thumbnail };
+
+  const estimatedValue = [item2.purchaseValue * 0.6, item2.purchaseValue * 0.8]; // fake estimation
+  const endWarrantyDate = moment(item2.purchaseDate).add(2, 'years'); // default French warranty
+
+  const item = {
+    ...req.body,
+    thumbnail: thumbnail,
+    id: uuidv4(),
+    estimatedValue,
+    endWarrantyDate,
+  };
+
   items.unshift(item);
   console.log(`  ${items.length} items`);
 
@@ -40,6 +65,14 @@ app.post('/items', async (req, res) => {
     name: item.name,
     purchaseValue: item.purchaseValue,
   });
+});
+app.get('/items/:id', (req, res) => {
+  const id = req.params.id;
+  const item = findItem(id);
+  if (!item) {
+    return res.status(404).send();
+  }
+  res.send(itemWithDetails(item));
 });
 
 app.listen(port, () => console.log(`Luko safe backend running on port ${port}!`));
